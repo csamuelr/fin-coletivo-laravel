2ccefa7e7210f73934ff204a232da26139d58a12
--replace-all <claesia.va@gmail.com>
2019-11-18 15:21:50 -0300

    Login e cadastro

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome');
-            $table->string('username')->unique();
-            $table->string('senha');
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+            $table->rememberToken();
         });
     }
 
